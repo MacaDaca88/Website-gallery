bf0e3baa9f0be1c06b01d9cc292bc9d833751468
Michael Duncan <114211123+MacaDaca88@users.noreply.github.com>
2022-11-07 22:37:29 +1300

    updates

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var popup = document.getElementById(popup);
+    popup.classList.toggle("show");
+}
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
